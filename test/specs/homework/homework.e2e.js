e1e153d3e2c2f40fd52025c5599e06adf9f496c2
mr Ink <inkoustova.m@gmail.com>
2023-10-24 12:17:11 +0200

    homework

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -1,6 +1,6 @@
 describe('Homework', async () => {
 
-    it('should open page and create screenshot', async () => {
+    it('should open page and create screenshot, lesson 1', async () => {
 
         await browser.reloadSession();
 
@@ -12,10 +12,11 @@ describe('Homework', async () => {
 
     });
 
-    it('should open registrace page and return selectors', async () => {
+    it('should open registrace page and return selectors, lesson 2', async () => {
 
         await browser.reloadSession();
         await browser.url('/registrace');
+
         const idNameSelector = $('#name');
         console.log(await idNameSelector.getHTML());
 
@@ -30,6 +31,31 @@ describe('Homework', async () => {
 
         const classSelector = $('.btn-primary');
         console.log(await classSelector.getHTML());
+
+        await browser.pause(5000);
+
+    });
+
+    it('should open registrace page and submit it, lesson 3', async () => {
+
+        await browser.reloadSession();
+        await browser.url('/registrace');
+        
+        const name = 'Lilie Liliovita'
+        const email = 'lilie@seznam.cz'
+        const password2 = 'Czechitas666'
+
+        const idNameSelector = $('#name');
+        const idEmailSelector = $('#email');
+        const idPasswordSelector = $('#password');
+        const idPasswordConfirmSelector = $('#password-confirm');
+        const SubmitButton = $('.btn-primary')
+
+        await idNameSelector.setValue(name);
+        await idEmailSelector.setValue(email);
+        await idPasswordSelector.setValue(password2);
+        await idPasswordConfirmSelector.setValue(password2);
+        await SubmitButton.click();
 
         await browser.pause(5000);
 
